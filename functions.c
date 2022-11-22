e30e09a49c21b46310edd9c9c5a30e53f36e6b42
KingHorey <oreoluwaakinbo.oa@gmail.com>
2022-11-22 19:36:17 +0100

    modified printf file

--- functions.c
+++ functions.c
@@ -1,12 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
-  *characters - prints out the number of charatacters
-  *
-  * Return: number of characters
+  * character - Function prints character when %c is used
+  * @parameter: variable that holds a va_list of arguments
+  * Return: returns a character value
   */
-char characters(va_list parameter)
-{
-	char c = va_arg( paramenter int);
 
+int character(va_list parameter)
+{
+	int x;
+
+	x = va_arg(parameter, int);
+	_putchar(x);
+	return (1);
+}
+
+/**
+  * string_character - Function prints string when %s is used
+  * @parameter: variable that holds a va_list of arguments
+  * Return: returns a character value
+  */
+
+int string_character(va_list parameter)
+{
+	int str_counter;
+	char *str;
+
+	str = va_arg(parameter, char*);
+
+	for (str_counter = 0; str[str_counter] != '\0'; str_counter++)
+	{
+		_putchar(str[str_counter]);
+	}
+	return (str_counter);
+}
 
