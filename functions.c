b30f21098e0db1a6b55fdb84aaa157914978d10a
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-22 20:43:38 +0000

    putchar file

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *characters - prints out the number of charatacters
+  *
+  * Return: number of characters
+  */
+char characters(va_list parameter)
+{
+	char c = va_arg( paramenter int);
+
+
